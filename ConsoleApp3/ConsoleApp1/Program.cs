16911e0bae8830b2144abbbce5be78847de7edcb
Bez23124 <157889944+Bez23124@users.noreply.github.com>
2024-11-26 11:07:29 +0200

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{ /// <summary>
+/// Найти минимальную величину их трех целых переменных a, b, c с 
+/// использованием вспомогательных методов (ввода аргументов, нахождения минимальной величины, вывода результата).
+/// </summary>
+    internal class Progra
+  
+        
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    /// <summary>
+    /// Найти минимальную величину из двух целых переменных a, b, 
+    /// используя вспомогательные методы (ввода аргументов, нахождения минимальной величины, вывода результата).
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = Input("a=");
+            int b = Input("b=");
+            int result = Min(a, b);
+            Output(result);
+            Console.ReadKey();
+
+        }
+        public static int Min(int a, int b)
+        {
+            return a < b ? a : b;
+        }
+            public static int Input(string x)
+            {
+                Console.Write(x);
+                return int.Parse(Console.ReadLine()); }
+            public static void Output(int result)
+            {
+                Console.WriteLine(result);
+            }
+
+                
+            }
+
+           
+
+            }
+            
+        
+    
